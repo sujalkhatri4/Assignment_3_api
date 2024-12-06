5dc06f3b3bffd41b4b76c60f7d2e0c5f7da84ce4
Sujal <sujalkhatri411@gmail.com>
2024-12-06 10:52:54 -0500

    installed mongoose

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 //import express
 const express = require('express');
-
+const bodyParser = require('body-parser');
 // initialize the express app
 const app = express();
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Define a root route
 app.get('/', (req, res) => {
     res.send('Welcome to the first program of Node.js Express');
 });
-
 
 //set the port
 const port = 8000;
