3051fe1c2cc91baf31b74049eb85c9b460bcea46
Sujal <sujalkhatri411@gmail.com>
2024-12-06 10:47:22 -0500

    line added to index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require('express');
 // initialize the express app
 const app = express();
 
+// Define a root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the first program of Node.js Express');
+});
+
+
 //set the port
 const port = 8000;
 
